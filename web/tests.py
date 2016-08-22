6b04521f00fec49579868eb811b1c6fc9e163641
Becky Smith <rebkwok@gmail.com>
2016-08-22 08:52:33 +0100

    Web tests

--- web/tests.py
+++ web/tests.py
@@ -1,3 +1,34 @@
-from django.test import TestCase
+from model_mommy import mommy
 
-# Create your tests here.
+from django.test import TestCase
+from django.contrib.sites.models import Site
+from django.core import management
+from django.core.urlresolvers import reverse
+
+from allauth.socialaccount.models import SocialApp
+
+
+def set_up_fb():
+    fbapp = mommy.make_recipe('entries.fb_app')
+    site = Site.objects.get_current()
+    fbapp.sites.add(site.id)
+
+
+class HomeViewTests(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.url = reverse('web:home')
+        set_up_fb()
+
+
+    def test_no_login_required(self):
+        self.client.get(self.url)
+
+
+class ManagementCommandsTests(TestCase):
+
+    def test_setup_fb(self):
+        self.assertEquals(SocialApp.objects.all().count(), 0)
+        management.call_command('setup_fb')
+        self.assertEquals(SocialApp.objects.all().count(), 1)
