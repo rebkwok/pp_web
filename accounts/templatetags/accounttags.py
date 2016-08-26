cd9957c13b16648b2631500b3eb4761dfeed0762
Becky Smith <rebkwok@gmail.com>
2016-08-26 14:01:04 +0100

    Remove unused template tag

--- accounts/templatetags/accounttags.py
+++ accounts/templatetags/accounttags.py
@@ -6,13 +6,6 @@ from ..models import OnlineDisclaimer
 register = template.Library()
 
 
-@register.assignment_tag
-def modify_redirect_field_value(ret_url):
-    if ret_url and ret_url in ['/accounts/password/change/', '/accounts/password/set/']:
-        return '/accounts/profile'
-    return ret_url
-
-
 @register.filter
 def has_disclaimer(user):
     disclaimer = OnlineDisclaimer.objects.filter(user=user)
