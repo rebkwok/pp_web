4cf0b69a029bd79e98ba60815514fe6821e6af36
Becky Smith <rebkwok@gmail.com>
2016-08-31 23:04:48 +0100

    fix db settings

--- poleperformance/settings.py
+++ poleperformance/settings.py
@@ -158,7 +158,7 @@ WSGI_APPLICATION = 'poleperformance.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=500),
+    'default': dj_database_url.config(),
 }
 
 
