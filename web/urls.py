c3b115adfa51bac03981e0426d973397907fd123
Becky Smith <rebkwok@gmail.com>
2019-09-25 19:32:56 +0100

    Add pages for 2018 and draft 2020 homepages

--- web/urls.py
+++ web/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import home
+from .views import home, home2018, home2020
 
 
 app_name = 'web'
@@ -8,5 +8,7 @@ app_name = 'web'
 
 urlpatterns = [
     path('', home, name='home'),
+    path('2018', home2018, name='home_2018'),
+    path('2020', home2020, name='home_2020'),
 ]
 
--- web/views.py
+++ web/views.py
@@ -2,5 +2,13 @@ from django.shortcuts import render
 
 
 def home(request):
+    return render(request, 'web/home2020_placeholder.html')
+
+
+def home2018(request):
+    return render(request, 'web/home2018.html')
+
+
+def home2020(request):
     return render(request, 'web/home2020.html')
 
